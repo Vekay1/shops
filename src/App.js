9f7e75340c17a2c8f1cad0a9ac419241706c5049
Volodymyr Fedorenko <40321991+Vekay1@users.noreply.github.com>
2024-12-19 16:09:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from "./components/footer/Footer";
 function App() {
   return (
     <div className="App">
-        
+        <Header />
         <Promo />
         <Brands />
         <Arrivals />
